78118f56a967aff360dc7ed21590647af11ff198
JustMonik3r <m.mintous@gmail.com>
2023-08-05 14:25:44 +0300

    mintus oop_encapsulation and inheritance homework done

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,3 +1,79 @@
-public class Hogwarts {
+import java.util.Objects;
 
+public abstract class Hogwarts {
+    private String name;
+    private int magicPotency;
+    private int apparitionDistance;
+
+    public Hogwarts(String name, int magicPotency, int apparitionDistance) {
+        this.name = name;
+        this.magicPotency = magicPotency;
+        this.apparitionDistance = apparitionDistance;
+    }
+
+    abstract public int calculateFacultyScore();
+
+    public void compareScore (Hogwarts hogwarts) {
+        if (this.getClass().equals(hogwarts.getClass())) {
+            compageSchoolScore(hogwarts);
+            compareFacultyScore(hogwarts);
+            System.out.println();
+        } else {
+            compageSchoolScore(hogwarts);
+            System.out.println();
+        }
+    }
+
+    private void compageSchoolScore (Hogwarts hogwarts) {
+        System.out.println("Сравнение общешкольных данных:");
+        int thisScore = this.getMagicPotency() + this.getApparitionDistance();
+        int otherScore = hogwarts.getMagicPotency() + hogwarts.getApparitionDistance();
+
+        if (thisScore > otherScore) {
+            System.out.println(this.getName() + " сильнее чем " + hogwarts.getName());
+        } else if (thisScore < otherScore) {
+            System.out.println(hogwarts.getName() + " сильнее чем " + this.getName());
+        } else {
+            System.out.println(this.getName() + " и " + hogwarts.getName() + " равны по силе");
+        }
+    }
+
+    private void compareFacultyScore(Hogwarts hogwarts) {
+        System.out.println("Сравнение факультетских данных:");
+        int thisScore = this.calculateFacultyScore();
+        int otherScore = hogwarts.calculateFacultyScore();
+
+        if (thisScore > otherScore) {
+            System.out.println(this.getName() + " лучше воплощает в себе качества факультета, чем " + hogwarts.getName());
+        } else if (thisScore < otherScore) {
+            System.out.println(hogwarts.getName() + " лучше воплощает в себе качества факультета, чем " + this.getName());
+        } else {
+            System.out.println(this.getName() + " и " + hogwarts.getName() + " образцовые представители факультета");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getMagicPotency() {
+        return magicPotency;
+    }
+
+
+    public int getApparitionDistance() {
+        return apparitionDistance;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Hogwarts{" +
+                "name='" + name + '\'' +
+                ", magicPotency=" + magicPotency +
+                ", apparitionDistance=" + apparitionDistance +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
 public class Main {
+
+    public static int generateRandomNumber (int randomInt) {
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        return number;
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Gryffindor harryPotter = new Gryffindor("Harry Potter", generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100));
+        Gryffindor hermioneGranger = new Gryffindor("Hermione Granger", generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Slytherin dracoMalfoy = new Slytherin("Draco Malfoy", generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100));
+        Slytherin gregoryGoyle = new Slytherin("Gregory Goyle", generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100), generateRandomNumber(100));
+
+        System.out.println(harryPotter);
+        System.out.println(hermioneGranger);
+        System.out.println(gregoryGoyle);
+        System.out.println(dracoMalfoy);
+        System.out.println();
+
+        harryPotter.compareScore(hermioneGranger);
+        dracoMalfoy.compareScore(gregoryGoyle);
+        harryPotter.compareScore(dracoMalfoy);
+        harryPotter.compareScore(gregoryGoyle);
     }
 }
